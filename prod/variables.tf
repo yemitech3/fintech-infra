d9eeff3b409da2833cb2ee4afdf20564c8e7e3aa
yemitech <206280379+yemitech3@users.noreply.github.com>
2026-01-14 02:27:00 +0000

    Update variables.tf

--- prod/variables.tf
+++ prod/variables.tf
@@ -75,7 +75,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 Key Pair name for SSH access"
   type        = string
-  default     = "key15"
+  default     = "key14"
 }
 
 ################################################################################
